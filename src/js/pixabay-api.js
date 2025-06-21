5dda803a99f2c7e5e433a7bcecdbdeea5b31e7bd
d-yv <yuri.dubrovin@gmail.com>
2025-06-21 03:15:31 +0300

    hide token

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export default async function getImagesByQuery(query, page = 1) {
-  const link = 'https://pixabay.com/api/';
+  const link = 'https://vercel-api-proxy-six-fawn.vercel.app/api/proxy';
   try {
     const response = await axios.get(link, {
       params: {
-        key: '50483673-3addba2370166e44fbbdc4d02',
+        source: 'pixabay',
         q: query,
         image_type: 'photo',
         orientation: 'horizontal',
